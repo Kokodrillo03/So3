81464a5aa5e2d2127dc72fef8773759681560756
krzychu <krzychu.biska2@gmail,com>
2022-05-04 00:34:47 +0200

    f

--- src/main.java
+++ src/main.java
@@ -1,12 +1,22 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args){
-        ArrayList<strona> strony = ciągi.generuj_ciag(1000,10);
-        System.out.println(Algorytmy.FIFO(strony,3));
-        System.out.println(Algorytmy.OPT(strony,3));
-        System.out.println(Algorytmy.RAND(strony,3));
-        System.out.println(Algorytmy.LRU(strony,3));
-        System.out.println(Algorytmy.AprLRU(strony,3));
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Podaj dlugosc ciagu testowego");
+        int dlug = Integer.parseInt(scan.nextLine());
+        System.out.println("Podaj romiar pamieci");
+        int pam = Integer.parseInt(scan.nextLine());
+        System.out.println("Podaj ilość ramek");
+        int ramki = Integer.parseInt(scan.nextLine());
+        ArrayList<strona> strony = ciągi.generuj_ciag(dlug,pam);
+        System.out.println("Optymalna ilosc bledow strony wynosi: "+Algorytmy.OPT(strony,ramki));
+        System.out.println("Ilość błędów storny dla algorytmu FIFO wynosi: "+Algorytmy.FIFO(strony,ramki));
+        System.out.println("Ilość błędów storny dla algorytmu RAND wynosi: "+Algorytmy.RAND(strony,ramki));
+        System.out.println("Ilość błędów storny dla algorytmu LRU wynosi: "+Algorytmy.LRU(strony,ramki));
+        System.out.println("Ilość błędów storny dla algorytmu optymalizowanego LRU wynosi: "+Algorytmy.AprLRU(strony,ramki));
     }
 }
